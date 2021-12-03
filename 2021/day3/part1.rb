d36273e2ec1d38b854fa1ff838d73a3bcfd0238d
Erik Westrup <erik.westrup@gmail.com>
2021-12-03 13:01:55 +0100

    Add 2021/03

---
+++ 2021/day3/part1.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+def s1
+  gamma = ARGF.each_line.map { |line| line.chomp.split('').map(&:to_i) }.transpose.map do |colchars|
+    colchars.sum >= colchars.length / 2 ? '1' : '0'
+  end.join.to_i(2)
+  epsilon = (~gamma) & ~((~0) << gamma.to_s(2).length)
+
+  puts gamma * epsilon
+end
+
+def s2
+  gamma = ''
+  epsilon = ''
+  ARGF.each_line.map { |line| line.chomp.split('').map(&:to_i) }.transpose.each do |colchars|
+    gamma += colchars.sum >= colchars.length / 2 ? '1' : '0'
+    epsilon += ((gamma[-1].to_i + 1) % 2).to_s
+  end
+  puts gamma.to_i(2) * epsilon.to_i(2)
+end
+
+# s1
+s2
---
+++ 2021/day3/part2.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+def most_common(array, col)
+  array.transpose.map { |colchars| (colchars.sum / colchars.length.to_f).round }[col]
+end
+
+gamma = ''
+epsilon = ''
+report = ARGF.each_line.map { |line| line.chomp.split('').map(&:to_i) }
+
+i = 0
+r = report
+until r.length == 1
+  r = r.select { |n| n[i] == most_common(r, i) }
+  i += 1
+end
+oxygen_rate = r[0].join.to_i(2)
+
+i = 0
+r = report
+until r.length == 1
+  r = r.select { |n| n[i] == (most_common(r, i) + 1) % 2 }
+  i += 1
+end
+scrub_rate = r[0].join.to_i(2)
+
+puts oxygen_rate * scrub_rate
