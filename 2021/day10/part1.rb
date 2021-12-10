89f136aaac23dbc3ebe2d5ce68563f9ff389f833
Erik Westrup <erik.westrup@gmail.com>
2021-12-10 11:33:54 +0100

    Add 2021/10

---
+++ 2021/day10/part1.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+PAIRS = {
+  '(' => ')',
+  '[' => ']',
+  '{' => '}',
+  '<' => '>'
+}
+
+POINTS = {
+  ')' => 3,
+  ']' => 57,
+  '}' => 1197,
+  '>' => 25_137
+}
+
+score = ARGF.each_line.map do |line|
+  stack = []
+  broken_char = nil
+  line.chomp.chars.each do |char|
+    if PAIRS.keys.include?(char)
+      stack.push(char)
+    else
+      opening = stack.pop
+      if PAIRS[opening] != char
+        broken_char = char
+        break
+      end
+    end
+  end
+  broken_char
+end.reject(&:nil?).map { |c| POINTS[c] }.sum
+
+puts score
---
+++ 2021/day10/part2.rb
@@ -0,0 +1,51 @@
+#!/usr/bin/env ruby
+
+class Array
+  def median
+    sorted = sort
+    if length.even?
+      (sorted[length / 2 - 1] + sorted[length / 2]) / 2.0
+    else
+      sorted[length / 2]
+    end
+  end
+end
+
+PAIRS = {
+  '(' => ')',
+  '[' => ']',
+  '{' => '}',
+  '<' => '>'
+}
+
+POINTS = {
+  ')' => 1,
+  ']' => 2,
+  '}' => 3,
+  '>' => 4
+}
+
+score = ARGF.each_line.map do |line|
+  stack = []
+  broken = false
+  score = 0
+  line.chomp.chars.each do |char|
+    if PAIRS.keys.include?(char)
+      stack.push(char)
+    else
+      opening = stack.pop
+      if PAIRS[opening] != char
+        broken = true
+        break
+      end
+    end
+  end
+  unless broken
+    stack.reverse_each do |open_char|
+      score = score * 5 + POINTS[PAIRS[open_char]]
+    end
+  end
+  score
+end.reject { |s| s == 0 }.median
+
+puts score
