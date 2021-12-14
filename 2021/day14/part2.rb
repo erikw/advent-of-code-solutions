cc117f15956be1c0ae112c8dbb58b64571f58c79
Erik Westrup <erik.westrup@gmail.com>
2021-12-14 13:22:33 +0100

    Refactor part2.rb

--- 2021/day14/part2.rb
+++ 2021/day14/part2.rb
@@ -2,33 +2,26 @@
 
 ITERATIONS = 40
 
-pair_counts = Hash.new(0) # C_1C_2 -> count
-polymer = ARGF.readline.chomp
-(0...polymer.length - 1).each do |i|
-  pair = polymer[i] + polymer[i + 1]
-  pair_counts[pair] += 1
-end
+pair_counts = Hash.new(0) # "char1+char2" -> count
+char_counts = Hash.new(0) # "char" ->count
+polymer = ARGF.readline.chomp.chars
+polymer.each_cons(2) { |a, b| pair_counts[a + b] += 1 }
+polymer.each { |c| char_counts[c] += 1 }
 
-productions = {}
+prod = {}
+ARGF.readline
 ARGF.each_line do |line|
-  pair, prod = line.chomp.split(' -> ')
-  productions[pair] = prod
+  prod.merge! [line.chomp.split(' -> ')].to_h
 end
 
-ITERATIONS.times do |_i|
+ITERATIONS.times do
   updates = Hash.new(0)
   pair_counts.each do |pair, count|
-    pair1 = pair[0] + productions[pair]
-    pair2 = productions[pair] + pair[1]
-    updates[pair1] += count
-    updates[pair2] += count
+    updates[pair[0] + prod[pair]] += count
+    updates[prod[pair] + pair[1]] += count
+    char_counts[prod[pair]]       += count
   end
   pair_counts = updates
 end
 
-res = pair_counts.flat_map do |pair, count|
-  [[pair[0], count], [pair[1], count]]
-end.group_by(&:first).values.map do |value|
-  (value.map(&:last).sum / 2.0).round
-end.minmax.sort.reverse.inject(&:-)
-puts res
+puts char_counts.values.minmax.sort.reverse.inject(&:-)
