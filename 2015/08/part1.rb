33517df2ce6cee2352c3eb13f7d95beea44fa656
Erik Westrup <erik.westrup@gmail.com>
2022-03-27 22:08:53 +0200

    Add 2015/08

---
+++ 2015/08/part1.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+chars_code = 0
+chars_mem = 0
+
+ARGF.each_line.map(&:chomp).each do |string|
+  chars_code += string.length
+  strmem = string.gsub(/\\x[0-9a-f]{2}/i, 'x').gsub(/\\"/, '"').gsub(/\\\\/, '\\')
+  chars_mem += strmem.length - 2
+end
+
+puts chars_code - chars_mem
---
+++ 2015/08/part2.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+chars_code = 0
+chars_encode = 0
+
+ARGF.each_line.map(&:chomp).each do |string|
+  chars_code += string.length
+  strencoded = string.gsub(/\\/, '\\\\\\').gsub(/"/, '\\"')
+  chars_encode += strencoded.length + 2
+end
+
+puts chars_encode - chars_code
