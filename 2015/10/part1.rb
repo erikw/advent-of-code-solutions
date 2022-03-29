e7357021feb9e7956d60ce125fe99965f39a0d43
Erik Westrup <erik.westrup@gmail.com>
2022-03-29 11:15:28 +0200

    Add 2015/10

---
+++ 2015/10/part1.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+seq = ARGF.readline.chomp.split('')
+
+40.times do
+  nseq = []
+  digit = seq.first
+  count = 1
+  (1...seq.length).each do |i|
+    if seq[i] == digit
+      count += 1
+    else
+      nseq << count.to_s << digit
+      digit = seq[i]
+      count = 1
+    end
+  end
+  nseq << count.to_s << digit
+  seq = nseq
+end
+puts seq.length
