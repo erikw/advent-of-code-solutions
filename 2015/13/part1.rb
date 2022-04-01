d97725386af3f1baf898712a45cb1545595d12f7
Erik Westrup <erik.westrup@gmail.com>
2022-04-01 11:44:55 +0200

    Add 2015/13

---
+++ 2015/13/part1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+happiness = Hash.new { |h, k| h[k] = {} }
+ARGF.each_line.map do |l|
+  l.gsub(/would/, '').gsub(/happiness units by sitting next to/, '').gsub(/\./, '').split
+end.each do |from, verb, units, to|
+  happiness[from][to] = verb == 'gain' ? units.to_i : -1 * units.to_i
+end
+
+max = happiness.keys.permutation.map do |seating|
+  seating.cycle.first(seating.length + 1).each_cons(2).map do |from, to|
+    happiness[from][to] + happiness[to][from]
+  end.sum
+end.max
+puts max
