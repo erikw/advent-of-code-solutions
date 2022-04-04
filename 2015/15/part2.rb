f0724300d1d578d95f7b901817120939c00aa9db
Erik Westrup <erik.westrup@gmail.com>
2022-04-04 13:51:53 +0200

    Add 2015/15

---
+++ 2015/15/part2.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+SPOONS_TOTAL = 100
+CALORIES_TARGET = 500
+
+def score(spoons, properties)
+  (0...properties[0].length - 1).map do |p|
+    term = (0...properties.length).sum do |i|
+      spoons[i] * properties[i][p]
+    end
+    term < 0 ? 0 : term
+  end.inject(&:*)
+end
+
+def calories(spoons, properties)
+  (0...properties.length).sum do |i|
+    spoons[i] * properties[i].last
+  end
+end
+
+properties = ARGF.each_line.map { |l| l.split(/ |,/).select { |e| e =~ /\d+/ }.map(&:to_i) }
+
+max_score = (0..SPOONS_TOTAL).to_a.combination(properties.length - 1).map do |indices|
+  spoons = indices.each_cons(2).map do |i1, i2|
+    i2 - i1
+  end.prepend(indices[0]).append(SPOONS_TOTAL - indices.last)
+  score(spoons, properties) * (calories(spoons, properties) == CALORIES_TARGET ? 1 : -1)
+end.max
+puts max_score
