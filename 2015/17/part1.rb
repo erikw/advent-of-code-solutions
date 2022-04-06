852f8a8caeefaf906191e9e8eee0f8ce8607ae17
Erik Westrup <erik.westrup@gmail.com>
2022-04-06 10:32:07 +0200

    Add 2015/17

---
+++ 2015/17/part1.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+
+# TARGET = 25
+TARGET = 150
+
+containers = ARGF.each_line.map(&:to_i)
+combinations = (1..containers.length).sum do |n|
+  containers.combination(n).map(&:sum).count { |s| s == TARGET }
+end
+puts combinations
---
+++ 2015/17/part2.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+# TARGET = 25
+TARGET = 150
+
+containers = ARGF.each_line.map(&:to_i)
+(1..containers.length).each do |n|
+  nbr = containers.combination(n).map(&:sum).select { |s| s == TARGET }.count
+  unless nbr.zero?
+    puts nbr
+    break
+  end
+end
