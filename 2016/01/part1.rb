84f0208f674e7ed42fd5eb70803263ac644d73f5
Erik Westrup <erik.westrup@gmail.com>
2022-04-18 10:44:01 +0200

    Add 2016/01

---
+++ 2016/01/part1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+row = 0
+col = 0
+facing = 0
+ARGF.readline.split(', ').map {|p| [p[0], p[1..].to_i] }.each do |turn, dist|
+  facing = (facing + (turn == 'R' ? 1 : -1)) % 4
+  case facing
+  when 0 then row += dist # N
+  when 1 then col += dist # E
+  when 2 then row -= dist # S
+  when 3 then col -= dist # W
+  end
+end
+puts row.abs + col.abs
---
+++ 2016/01/part2_set.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+row = 0
+col = 0
+facing = 0
+visited = Set.new([row, col])
+ARGF.readline.split(', ').map {|p| [p[0], p[1..].to_i] }.each do |turn, dist|
+  facing = (facing + (turn == 'R' ? 1 : -1)) % 4
+  row_inc, col_inc = 0, 0
+  case facing
+  when 0 then row_inc = 1  # N
+  when 1 then col_inc = 1  # E
+  when 2 then row_inc = -1 # S
+  when 3 then col_inc = -1 # W
+  end
+  dist.times do
+    row += row_inc
+    col += col_inc
+    loc = [row, col]
+    if visited.include? loc
+      puts row.abs + col.abs
+      exit
+    end
+    visited <<  loc
+  end
+end
