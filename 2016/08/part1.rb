eb3e04d559a95f9bcf4f0c0934bf9a2462f9aee5
Erik Westrup <erik.westrup@gmail.com>
2022-04-21 12:56:47 +0200

    Add 2016/08

---
+++ 2016/08/part1.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+
+#WIDTH = 7
+#HEIGHT = 3
+ WIDTH = 50
+ HEIGHT = 6
+
+OFF = '.'
+ON = 'x'
+
+
+display = Array.new(HEIGHT) { Array.new(WIDTH, OFF) }
+
+ARGF.each_line.map(&:split).each do |args|
+  case args
+  in ["rect", dim]
+    a, b = dim.split('x').map(&:to_i)
+    (0...b).each do |row|
+      (0...a).each do |col|
+        display[row][col] = ON
+      end
+    end
+  in ["rotate", "row", row, "by", rot]
+    row = row[2..].to_i
+    rot = rot.to_i
+    display[row].rotate!(-rot)
+  in ["rotate", "column", col, "by", rot]
+    col = col[2..].to_i
+    rot = rot.to_i
+    display = display.transpose
+    display[col].rotate!(-rot)
+    display = display.transpose
+  end
+end
+
+puts display.sum {|row| row.count {|c| c == ON }}
---
+++ 2016/08/part2.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+WIDTH = 50
+HEIGHT = 6
+
+OFF = ' '
+ON = '0'
+
+def print_disp(disp)
+  disp.each do |row|
+    puts row.join
+  end
+end
+
+display = Array.new(HEIGHT) { Array.new(WIDTH, OFF) }
+
+ARGF.each_line.map(&:split).each do |args|
+  case args
+  in ["rect", dim]
+    a, b = dim.split('x').map(&:to_i)
+    (0...b).each do |row|
+      (0...a).each do |col|
+        display[row][col] = ON
+      end
+    end
+  in ["rotate", "row", row, "by", rot]
+    row = row[2..].to_i
+    rot = rot.to_i
+    display[row].rotate!(-rot)
+  in ["rotate", "column", col, "by", rot]
+    col = col[2..].to_i
+    rot = rot.to_i
+    display = display.transpose
+    display[col].rotate!(-rot)
+    display = display.transpose
+  end
+end
+
+print_disp(display)
