d883f62c9be98d23e2950a30dad6acc50a66eaaa
Erik Westrup <erik.westrup@gmail.com>
2022-05-04 20:32:17 +0200

    Add 2016/15

---
+++ 2016/15/part1.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+discs = ARGF.each_line.map { |l| l.scan(/ \d+/).map(&:to_i) }
+
+(0...discs.length).each do |i|
+  discs[i][1] = (discs[i][1] + 1 + i) % discs[i][0]
+end
+
+time = 0
+until discs.map(&:last).all?(&:zero?)
+  time += 1
+  (0...discs.length).each do |i|
+    discs[i][1] = (discs[i][1] + 1) % discs[i][0]
+  end
+end
+puts time
