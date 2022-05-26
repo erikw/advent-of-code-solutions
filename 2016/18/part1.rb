0c2193e923948001b280f081e99889efcc64a6bb
Erik Westrup <erik.westrup@gmail.com>
2022-05-26 20:29:41 +0200

    Add 2016/18

---
+++ 2016/18/part1.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+# ROWS = 3
+# ROWS = 10
+ROWS = 40
+TILE_SAFE = '.'
+TILE_TRAP = '^'
+
+tiles = [[TILE_SAFE] + ARGF.readline.chomp.split('') + [TILE_SAFE]]
+cols = tiles[0].length
+
+(1...ROWS).each do |row|
+  tiles << Array.new(cols)
+  (0...cols).each do |col|
+    tiles[row][col] = if [0, cols - 1].include?(col) || tiles[row - 1][col - 1] == tiles[row - 1][col + 1]
+                        TILE_SAFE
+                      else
+                        TILE_TRAP
+                      end
+  end
+end
+
+tiles.each do |row|
+  puts row[1..-2].join
+end
+
+cnt = tiles.sum do |row|
+  row[1..-2].count do |tile|
+    tile == TILE_SAFE
+  end
+end
+
+puts cnt
