43014cce9193ef89a76b7c272e25b418ce84aee0
Erik Westrup <erik.westrup@gmail.com>
2022-05-31 12:36:49 +0200

    Yearly solved puzzles stats

--- solved_puzzles.sh
+++ solved_puzzles.sh
@@ -1,4 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+# Assumption: commited README.txt means a solved puzzle.
 
-# Ref: https://stackoverflow.com/a/69326159/265508
-find . -name README.txt -exec printf %c {} + | wc -c | tr -d ' '
+nbr_solved() {
+	local path="$1"
+	test -z ${path} && path=.
+
+	# Ref: https://stackoverflow.com/a/69326159/265508
+	find "$path" -name README.txt -exec printf %c {} + | wc -c | tr -d ' '
+}
+
+years=$(find . -type d -regex "\./*[0-9]*" -maxdepth 1 | grep -oE "\d+" | sort)
+for year in $years; do
+	solved=$(nbr_solved $year)
+	if [ $solved  = "25" ]; then
+		solved="25 ⭐️"
+	fi
+	printf "%s: %s\n" $year "$solved"
+done
+
+printf "\nTotal: %d\n" $(nbr_solved)
