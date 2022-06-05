38df67df810496989426611d2f3deac58635b5a6
Erik Westrup <erik.westrup@gmail.com>
2022-06-05 11:48:37 +0200

    Refactor 2016/12

--- 2016/12/part2.rb
+++ 2016/12/part2.rb
@@ -3,7 +3,6 @@
 require_relative 'computer'
 
 program = ARGF.readlines.map { |l| l.chomp.split }
-computer = Computer.new
-computer.registers['c'] = 1
+computer = Computer.new(c: 1)
 computer.execute(program)
 puts computer.registers['a']
