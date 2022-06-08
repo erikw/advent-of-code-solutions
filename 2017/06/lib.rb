c31d04b852f8932fd97bc3ae7378d75e5d8bf020
Erik Westrup <erik.westrup@gmail.com>
2022-06-08 20:37:39 +0200

    Add 2017/06

---
+++ 2017/06/lib.rb
@@ -0,0 +1,15 @@
+def largest_bank(banks)
+  banks.index(banks.max)
+end
+
+def realloc(banks)
+  largest = largest_bank(banks)
+  blocks = banks[largest]
+  banks[largest] = 0
+
+  i = (largest + 1) % banks.length
+  blocks.times do
+    banks[i] += 1
+    i = (i + 1) % banks.length
+  end
+end
