c08f4bf67ad56c3cae015ee6bf8bbd97f2c34d11
Erik Westrup <erik.westrup@gmail.com>
2022-06-08 19:53:16 +0200

    Add 2017/05

---
+++ 2017/05/part1.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+jumps = ARGF.each_line.map(&:to_i)
+
+pos = 0
+count = 0
+while pos.between?(0, jumps.length - 1)
+  jumps[pos] += 1
+  pos += jumps[pos] - 1
+  count += 1
+end
+puts count
