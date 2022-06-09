7321cb8042efc4e92d2a1e1c23f04987f1be6b8d
Erik Westrup <erik.westrup@gmail.com>
2022-06-09 12:27:07 +0200

    Add 2017/07

---
+++ 2017/07/lib.rb
@@ -0,0 +1,15 @@
+class Program
+  attr_reader :name, :weight
+  attr_accessor :subtowers
+
+  def initialize(name, weight)
+    @name = name
+    @weight = weight
+    @subtowers = []
+  end
+
+  def to_s
+    "Program{#{@name}, #{@weight}, #{@subtowers.map(&:name)}}"
+  end
+  alias inspect to_s
+end
---
+++ 2017/07/part2.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib'
+
+def find_unbalanced(program)
+  weights = []
+  program.subtowers.each do |subprogram|
+    weight, adjusted = find_unbalanced(subprogram)
+    return nil, adjusted unless adjusted.nil?
+
+    weights << [weight, subprogram]
+  end
+
+  tally = weights.map(&:first).tally.sort_by(&:last).map(&:first)
+  if tally.length == 2
+    program = weights.select { |w, _p| w == tally.first }.first.last
+    diff = tally.reverse.inject(&:-)
+    [nil, program.weight + diff]
+  else
+    [program.weight + weights.map(&:first).sum, nil]
+  end
+end
+
+program_parts = ARGF.each_line.map { |line| line.gsub(/[->(),]/, '').split }
+
+programs = {}
+program_parts.each do |parts|
+  programs[parts[0]] = Program.new(parts[0], parts[1].to_i)
+end
+
+root_programs = programs.dup
+
+program_parts.reject { |p| p.length == 2 }.each do |parts|
+  prog = programs[parts[0]]
+  parts[2..].each do |subprogram|
+    prog.subtowers << root_programs.delete(subprogram)
+  end
+end
+
+puts find_unbalanced(root_programs.values.first).last
