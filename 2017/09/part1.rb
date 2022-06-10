a71ec3dd24c7d6aeb4b7ea6d864b7aee9802fbc8
Erik Westrup <erik.westrup@gmail.com>
2022-06-10 10:59:49 +0200

    Add 2017/09

---
+++ 2017/09/part1.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+total_score = 0
+score = 0
+skip_next = false
+in_garbage = false
+ARGF.readline.chomp.each_char do |c|
+  if skip_next
+    skip_next = false
+    next
+  end
+
+  if c == '!'
+    skip_next = true
+  elsif in_garbage
+    in_garbage = false if c == '>'
+  elsif c == '{'
+    score += 1
+    total_score += score
+  elsif c == '}'
+    score -= 1
+  elsif c == '<'
+    in_garbage = true
+  end
+end
+
+puts total_score
