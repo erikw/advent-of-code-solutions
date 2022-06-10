cf048659c831283b6306ac3476133949f2128712
Erik Westrup <erik.westrup@gmail.com>
2022-06-10 13:57:47 +0200

    Add 2017/10

---
+++ 2017/10/part1.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+require_relative 'knot_hash'
+
+# kn = KnotHash::KnotHash.new(5)
+kn = KnotHash::KnotHasher.new
+
+lengths = ARGF.readline.split(',').map(&:to_i)
+puts kn.simple_hash(lengths)
