a4aec1b4020c0e52ddd3510cb542f70bc5f1adab
Erik Westrup <erik.westrup@gmail.com>
2022-06-12 01:56:39 +0200

    Add 2017/12

---
+++ 2017/12/part2.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+def component(edges, from)
+  que = Thread::Queue.new([from])
+  component = Set.new
+
+  until que.empty?
+    program = que.pop
+    component << program
+    edges[program].each { |to| que << to unless component.include?(to) }
+  end
+  component
+end
+
+def connected_components(edges)
+  nodes = Set.new(edges.keys)
+  components = []
+  until nodes.empty?
+    program = nodes.to_a.first
+    nodes.delete(program)
+    component = component(edges, program)
+    components << component
+    nodes -= component
+  end
+  components
+end
+
+pipes = Hash.new { |h, k| h[k] = Set.new }
+ARGF.each_line do |line|
+  parts = line.chomp.split('<->')
+  from = parts[0].to_i
+  parts[1].split(',').each do |to|
+    pipes[from] << to.to_i
+  end
+end
+
+puts connected_components(pipes).length
