2817579b0f5ad897688abeff12b117bba8ce4e59
Erik Westrup <erik.westrup@gmail.com>
2022-06-12 23:38:19 +0200

    Add 2017/14

---
+++ 2017/14/part1.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require_relative '../10/knot_hash'
+
+GRID_DIM = 128
+
+key = ARGF.readline.chomp
+hasher = KnotHash::KnotHasher.new
+ones = (0...GRID_DIM).sum do |i|
+  input = "#{key}-#{i}".each_char.map(&:ord)
+  hash =  hasher.hash(input)
+  bin = hash.each_char.map { |c| c.hex.to_s(2).rjust(4, '0') }.join
+  bin.each_char.count { |d| d == '1' }
+end
+puts ones
