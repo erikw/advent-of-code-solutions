c7422dd3f7b66d7b21084087317d41c9a7b07eb7
Erik Westrup <erik.westrup@gmail.com>
2022-06-12 11:39:04 +0200

    Add 2017/13

---
+++ 2017/13/lib.rb
@@ -0,0 +1,14 @@
+def scanner_pos(range, time)
+  time %= 2 * range - 2
+  if time >= range
+    2 * range - time - 2
+  else
+    time
+  end
+end
+
+def severeties(layers, wait = 0)
+  layers.map do |depth, range|
+    depth * range if scanner_pos(range, depth + wait) == 0
+  end.compact
+end
