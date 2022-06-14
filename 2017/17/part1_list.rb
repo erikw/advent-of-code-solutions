afafb3fb3263ef93a163169be270e8b2b28fdf3d
Erik Westrup <erik.westrup@gmail.com>
2022-06-14 08:43:42 +0200

    Add 2017/17

---
+++ 2017/17/part1_list.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+VALUES = 2018
+
+class Node
+  attr_reader :value
+  attr_accessor :next
+
+  def initialize(value = 0, nextn = nil)
+    @value = value
+    @next = nextn
+  end
+end
+
+steps = ARGF.readline.to_i
+
+node = Node.new
+node.next = node
+
+(1...VALUES).each do |v|
+  steps.times do
+    node = node.next
+  end
+  node_new = Node.new(v)
+  node_new.next = node.next
+  node.next = node_new
+  node = node_new
+end
+
+puts node.next.value
