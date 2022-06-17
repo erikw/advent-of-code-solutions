a9a6797de9363c9880ca7867d8fcb65493c80b2b
Erik Westrup <erik.westrup@gmail.com>
2022-06-17 23:25:43 +0200

    Add 2018/03

---
+++ 2018/03/part2.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+# SIDE = 11
+SIDE = 1000
+CLAIM_PATTERN = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/
+
+fabric = Array.new(SIDE) { Array.new(SIDE) }
+nonoverlapping = Set.new
+
+ARGF.each_line do |line|
+  id, left, top, wide, tall = line.match(CLAIM_PATTERN)[1..].map(&:to_i)
+  nonoverlapping << id
+  (top...(top + tall)).each do |row|
+    (left...(left + wide)).each do |col|
+      if fabric[row][col].nil?
+        fabric[row][col] = id
+      else
+        nonoverlapping.delete(id)
+        nonoverlapping.delete(fabric[row][col])
+      end
+    end
+  end
+end
+
+puts nonoverlapping.to_a.first
