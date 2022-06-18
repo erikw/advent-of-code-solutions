41248212eb791d458f80d3f3e8fa420f74cd156c
Erik Westrup <erik.westrup@gmail.com>
2022-06-18 09:33:42 +0200

    Add 2018/04

---
+++ 2018/04/part1.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require 'time'
+
+guard_id = nil
+sleep_start = nil
+
+guard_sleeps = Hash.new { |h, k| h[k] = Array.new(60, 0) }
+ARGF.each_line.map do |line|
+  time, event = line.split(']')
+  time = Time.parse(time[1..])
+  event.strip!
+  [time, event]
+end.sort_by(&:first).each do |time, event|
+  case event
+  when /Guard #(\d+) begins shift/
+    guard_id = Regexp.last_match(1).to_i
+  when 'falls asleep'
+    sleep_start = time.min
+  when 'wakes up'
+    (sleep_start...time.min).each do |min|
+      guard_sleeps[guard_id][min] += 1
+    end
+  end
+end
+
+ans = guard_sleeps.map do |guard_id, minutes|
+  total = minutes.sum
+  most_min = minutes.each_with_index.max_by(&:first).last
+  [total, most_min, guard_id]
+end.max_by(&:first).drop(1).inject(&:*)
+puts ans
