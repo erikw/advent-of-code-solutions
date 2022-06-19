4c94e42bcb7f2fbd1458394a3e67072180de51de
Erik Westrup <erik.westrup@gmail.com>
2022-06-19 18:23:00 +0200

    Add 2018/06

---
+++ 2018/06/part2.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+# MAX_DIST = 32
+MAX_DIST = 10_000
+
+def manhattan_distance(p1, p2)
+  p1.zip(p2).map { |a, b| (a - b).abs }.sum
+end
+
+def dist_to_points(points, point)
+  points.map { |p| manhattan_distance(p, point) }.sum
+end
+
+points = ARGF.each_line.map { |l| l.split.map(&:to_i) }
+x_max = points.map(&:first).max
+y_max = points.map(&:last).max
+
+region = (0..y_max).sum do |y|
+  (0..x_max).count do |x|
+    dist_to_points(points, [x, y]) < MAX_DIST
+  end
+end
+puts region
