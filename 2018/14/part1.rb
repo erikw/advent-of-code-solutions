95ba2236660bd9811040e30ebe38b115784a7ddd
Erik Westrup <erik.westrup@gmail.com>
2022-06-22 11:34:09 +0200

    Add 2018/14

---
+++ 2018/14/part1.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+
+RECEPIES_NEEDED = 10
+
+nbr_recipies = ARGF.gets.to_i
+recepies = [3, 7]
+elf_pos = [0, 1]
+
+until recepies.length >= nbr_recipies + RECEPIES_NEEDED
+  sum = recepies[elf_pos[0]] + recepies[elf_pos[1]]
+  recepies << sum / 10 if sum > 9
+  recepies << sum % 10
+  2.times do |i|
+    elf_pos[i] = (elf_pos[i] + 1 + recepies[elf_pos[i]]) % recepies.length
+  end
+end
+
+puts recepies[nbr_recipies, RECEPIES_NEEDED].join
---
+++ 2018/14/part2_str.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+SKIP_ITERATIONS = 20_000_000
+
+input = ARGF.gets.chomp
+recepies = '37'
+elf_pos = [0, 1]
+
+# until recepies.length >= 7 && recepies[-7..].include?(input)
+SKIP_ITERATIONS.times do
+  recepies += (recepies[elf_pos[0]].to_i + recepies[elf_pos[1]].to_i).to_s
+  2.times do |i|
+    elf_pos[i] = (elf_pos[i] + 1 + recepies[elf_pos[i]].to_i) % recepies.length
+  end
+end
+
+puts recepies.index(input)
