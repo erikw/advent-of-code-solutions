0ecc8230e0cf1552e3d308617b5f78dca2212a33
Erik Westrup <erik.westrup@gmail.com>
2022-06-25 21:48:29 +0200

    Add 2018/18

---
+++ 2018/18/part2.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib'
+require 'set'
+
+MINUTES = 1_000_000_000
+
+acres = ARGF.each_line.map(&:chomp)
+
+acres_seen = Set.new
+min = 0
+until acres_seen.include?(acres)
+  acres_seen << acres
+  acres = iterate_acres(acres)
+  min += 1
+end
+
+min *= MINUTES / min
+(min...MINUTES).each do
+  acres = iterate_acres(acres)
+end
+
+wood = acres.join.chars.count(SYM_TREES)
+lumber = acres.join.chars.count(SYM_LUMBER)
+puts wood * lumber
