e472273e6d5abd9cfb4fe553724f61e7ad9bef55
Erik Westrup <erik.westrup@gmail.com>
2022-06-25 23:35:21 +0200

    Add 2018/19

---
+++ 2018/19/part2.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require_relative 'computer'
+
+instructions = ARGF.each_line.map { |l| l.chomp.split }
+# puts Computer.new.execute(instructions).registers[0]
+
+# Hats off to https://www.reddit.com/r/adventofcode/comments/a7j9zc/comment/ec45g4d/
+a = instructions[22][2].to_i
+b = instructions[24][2].to_i
+n = 836 + 22 * a + b + 10_550_400
+sqn = (n**0.5).to_i
+ans = (1...(sqn + 1)).map do |d|
+  n % d == 0 ? d + n / d : 0
+end.sum
+ans = sqn**2 == n ? ans - sqn : ans
+puts ans
