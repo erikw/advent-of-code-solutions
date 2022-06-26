d6c0e6780173169a8bce2ab75bc52cc095032347
Erik Westrup <erik.westrup@gmail.com>
2022-06-27 00:48:16 +0200

    Add 2018/21

---
+++ 2018/21/part2.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+
+require_relative 'computer'
+
+instructions = ARGF.each_line.map { |l| l.chomp.split }
+puts Computer.new.execute(instructions, watch_line: 28, watch_action: :detect_repeat, watch_reg: 1).registers[1]
