b638e97dfd159da446f9b1dcad9fac58b0578ad6
Erik Westrup <erik.westrup@gmail.com>
2022-06-26 23:52:10 +0200

    Cleanup

--- 2018/19/computer.rb
+++ 2018/19/computer.rb
@@ -15,38 +15,26 @@ class Computer
       instruction = instructions[@ip][0]
       a, b, c = instructions[@ip][1..3].map(&:to_i)
       @registers[@ip_register] = @ip
-      if @registers[@ip_register] == 3
-        if @registers[1] % @registers[5] == 0
-          @registers[3] = @registers[1]
-          @registers[4] = 1
-          @registers[@ip_register] = 7
-        else
-          @registers[3] = @registers[1] + 1
-          @registers[4] = 1
-          @registers[@ip_register] = 12
-        end
-      else
-        case instruction
-        when 'addr' then op_addr(a, b, c)
-        when 'addi' then op_addi(a, b, c)
-        when 'mulr' then op_mulr(a, b, c)
-        when 'muli' then op_muli(a, b, c)
-        when 'banr' then op_banr(a, b, c)
-        when 'bani' then op_bani(a, b, c)
-        when 'borr' then op_borr(a, b, c)
-        when 'bori' then op_bori(a, b, c)
-        when 'setr' then op_setr(a, b, c)
-        when 'seti' then op_seti(a, b, c)
-        when 'gtir' then op_gtir(a, b, c)
-        when 'gtri' then op_gtri(a, b, c)
-        when 'gtrr' then op_gtrr(a, b, c)
-        when 'eqir' then op_eqir(a, b, c)
-        when 'eqri' then op_eqri(a, b, c)
-        when 'eqrr' then op_eqrr(a, b, c)
-        end
-        @ip = @registers[@ip_register]
-        @ip += 1
+      case instruction
+      when 'addr' then op_addr(a, b, c)
+      when 'addi' then op_addi(a, b, c)
+      when 'mulr' then op_mulr(a, b, c)
+      when 'muli' then op_muli(a, b, c)
+      when 'banr' then op_banr(a, b, c)
+      when 'bani' then op_bani(a, b, c)
+      when 'borr' then op_borr(a, b, c)
+      when 'bori' then op_bori(a, b, c)
+      when 'setr' then op_setr(a, b, c)
+      when 'seti' then op_seti(a, b, c)
+      when 'gtir' then op_gtir(a, b, c)
+      when 'gtri' then op_gtri(a, b, c)
+      when 'gtrr' then op_gtrr(a, b, c)
+      when 'eqir' then op_eqir(a, b, c)
+      when 'eqri' then op_eqri(a, b, c)
+      when 'eqrr' then op_eqrr(a, b, c)
       end
+      @ip = @registers[@ip_register]
+      @ip += 1
     end
 
     self
