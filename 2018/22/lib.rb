7db83ab83f76dfdecade3cabb0de960801abd1fd
Erik Westrup <erik.westrup@gmail.com>
2022-06-27 15:24:52 +0200

    Add 2018/22

---
+++ 2018/22/lib.rb
@@ -0,0 +1,92 @@
+SYM_ROCKY = '.'
+SYM_WET = '='
+SYM_NARROW = '|'
+SYM_MOUTH = 'M'
+SYM_TARGET = 'T'
+SYM_PATH = 'P'
+
+TYPE_ROCKY = 0
+TYPE_WET = 1
+TYPE_NARROW = 2
+
+TYPE2SYM = {
+  TYPE_ROCKY => SYM_ROCKY,
+  TYPE_WET => SYM_WET,
+  TYPE_NARROW => SYM_NARROW
+}
+
+POS_MOUTH = 0 + 0i
+
+EROSION_MOD = 20_183
+GEO_INDEX_X_MUL = 16_807
+GEO_INDEX_Y_MUL = 48_271
+
+def read_input
+  depth = ARGF.readline.split[1].to_i
+  target = Complex(*ARGF.readline.split[1].split(',').map(&:to_i))
+  [depth, target]
+end
+
+def geo_index(erosion_levels, target, depth, pos)
+  if [POS_MOUTH, target].include?(pos)
+    0
+  elsif pos.imag.zero?
+    pos.real * GEO_INDEX_X_MUL
+  elsif pos.real.zero?
+    pos.imag * GEO_INDEX_Y_MUL
+  else
+    erosion_level(erosion_levels, target, depth, pos - 1) * erosion_level(erosion_levels, target, depth, pos - 1i)
+  end
+end
+
+def erosion_level(erosion_levels, target, depth, pos)
+  unless erosion_levels.key?(pos)
+    erosion_levels[pos] = (geo_index(erosion_levels, target, depth, pos) + depth) % EROSION_MOD
+  end
+  erosion_levels[pos]
+end
+
+def erosion_levels(target, depth)
+  levels = {}
+
+  (POS_MOUTH.imag..target.imag).each do |y|
+    (POS_MOUTH.real..target.real).each do |x|
+      pos = Complex(x, y)
+      levels[pos] = erosion_level(levels, target, depth, pos)
+    end
+  end
+  levels
+end
+
+def print_cave(erosion_levels, target, depth, path = nil)
+  if path.nil?
+    y_max = target.imag
+    x_max = target.real
+  else
+    y_max = path.map(&:imag).max
+    x_max = path.map(&:real).max
+  end
+  (POS_MOUTH.imag..y_max).each do |y|
+    (POS_MOUTH.real..x_max).each do |x|
+      pos = Complex(x, y)
+      if pos == POS_MOUTH
+        print SYM_MOUTH
+      elsif pos == target
+        print SYM_TARGET
+      elsif !path.nil? && path.include?(pos)
+        print SYM_PATH
+      else
+        print TYPE2SYM[type(erosion_levels, target, depth, pos)]
+      end
+    end
+    puts
+  end
+end
+
+def type(erosion_levels, target, depth, pos)
+  erosion_level(erosion_levels, target, depth, pos) % 3
+end
+
+def types(erosion_levels, target, depth)
+  erosion_levels.map { |pos, _lvl| [pos, type(erosion_levels, target, depth, pos)] }.to_h
+end
---
+++ 2018/22/part2.rb
@@ -0,0 +1,92 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib'
+
+require 'set'
+require 'lazy_priority_queue'
+
+class LazyPriorityQueue
+  def upsert(element, key)
+    change_priority(element, key)
+  rescue StandardError
+    enqueue(element, key)
+  end
+end
+
+TIME_MOVE = 1
+TIME_SWITCH_TOOL = 7
+
+TYPE_TOOLS = {
+  TYPE_ROCKY => %i[tool_climbing tool_torch],
+  TYPE_WET => %i[tool_climbing tool_neither],
+  TYPE_NARROW => %i[tool_torch tool_neither]
+}
+
+NEIGHBORS_DELTAS = [-1, 1, -1i, 1i]
+
+# Modified Dijkstra's algorithm from given starting coordinate:
+# - Only adds nodes to queue as they are discovered, to avoid having to BFS all currently available positions before the algoritm.
+# - Quit early when finding target, as the cave could be infinite
+# - Each node defined by positon and tool type when entring it.
+# - Adding self-edge to each node for a change of tool.
+def dijksta(erosions, target, depth)
+  dist = Hash.new(Float::INFINITY)
+  dist[[POS_MOUTH, :tool_torch]] = 0
+  prev = {}
+  visited = Set.new
+
+  q = MinPriorityQueue.new
+  q.push([POS_MOUTH, :tool_torch], dist[[POS_MOUTH, :tool_torch]])
+  until q.empty?
+    pos, tool = q.pop
+    return [dist, prev] if pos == target && tool == :tool_torch
+
+    visited << [pos, tool]
+    type_cur = type(erosions, target, depth, pos)
+
+    # Add edge to self with tooling change
+    other_tool = (TYPE_TOOLS[type_cur] - [tool]).first
+    alt_self = dist[[pos, tool]] + TIME_SWITCH_TOOL
+    if !visited.include?([pos, other_tool]) && alt_self < dist[[pos, other_tool]]
+      dist[[pos, other_tool]] = alt_self
+      prev[[pos, other_tool]] = [pos, tool]
+      q.upsert([pos, other_tool], dist[[pos, other_tool]])
+    end
+
+    NEIGHBORS_DELTAS.map do |delta|
+      pos_n = pos + delta
+      next if pos_n.real.negative? || pos_n.imag.negative?
+
+      type_n = type(erosions, target, depth, pos_n)
+      alt = dist[[pos, tool]] + TIME_MOVE
+      next unless !visited.include?([pos_n, tool]) && TYPE_TOOLS[type_n].include?(tool) && alt < dist[[pos_n, tool]]
+
+      dist[[pos_n, tool]] = alt
+      prev[[pos_n, tool]] = [pos, tool]
+      q.upsert([pos_n, tool], alt)
+    end
+  end
+  [dist, prev]
+end
+
+def construct_path(prev, source, target)
+  path = []
+  u = target
+  t = :tool_torch
+  if prev.key?([u, t]) || u == source && t == :tool_torch
+    until u.nil?
+      path.unshift(u)
+      u, t = prev[[u, t]]
+    end
+  end
+  path
+end
+
+depth, target = read_input
+erosions = erosion_levels(target, depth)
+dist, prev = dijksta(erosions, target, depth)
+
+# path = construct_path(prev, POS_MOUTH, target)
+# print_cave(erosions, target, depth, path)
+
+puts dist[[target, :tool_torch]]
