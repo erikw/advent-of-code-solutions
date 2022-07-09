63e462472af7a165cd08af8c2df138f263784f5d
Erik Westrup <erik.westrup@gmail.com>
2022-07-09 16:35:53 +0200

    Add 2019/04

---
+++ 2019/04/part1.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+def valid(num)
+  repeated = false
+  increasing = true
+
+  last = 10
+  until num == 0
+    digit = num % 10
+
+    repeated = true if digit == last
+    increasing = false if digit > last
+
+    num /= 10
+    last = digit
+  end
+  repeated && increasing
+end
+
+lo, hi = ARGF.readline.chomp.split('-').map(&:to_i)
+num_valid = (lo..hi).count { |pw| valid(pw) }
+puts num_valid
