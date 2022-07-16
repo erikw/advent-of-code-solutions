6ce82d0b2c0dc769a1a232d0e508087ba54ac02e
Erik Westrup <erik.westrup@gmail.com>
2022-07-16 16:47:05 +0200

    Array impl 2019/05

--- 2019/05/part2_pipe.rb
+++ 2019/05/part2_pipe.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
-require_relative 'computer'
+require_relative 'computer_pipe'
 
 intcode = ARGF.readline.split(',').map(&:to_i)
 computer = Computer.new
-computer.stdin << 5
+computer.stdin << "5\n"
 computer.execute(intcode)
-puts computer.stdout.last
+puts computer.stdout.readlines.last
