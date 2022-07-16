52c75746afcfae7edd2bed6f066bb903a103ea6a
Erik Westrup <erik.westrup@gmail.com>
2022-07-16 19:01:05 +0200

    Add 2019/08

---
+++ 2019/08/part1.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+
+IMG_W = 25
+IMG_H = 6
+
+ans = ARGF.readline.chomp.split('').map(&:to_i).each_slice(IMG_W * IMG_H).min_by do |layer|
+  layer.count { |d| d.zero? }
+end.tally.reject { |k, _v| k == 0 }.values.inject(&:*)
+puts ans
