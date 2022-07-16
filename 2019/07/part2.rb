228a92f6a3d1be16b82a43cbcfe8f4ca98b80610
Erik Westrup <erik.westrup@gmail.com>
2022-07-16 16:50:54 +0200

    Add 2019/07

---
+++ 2019/07/part2.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+
+require_relative 'computer'
+
+NUM_AMPLIFIERS = 5
+PHASE_RANGE = 5..9
+
+intcode = ARGF.readline.split(',').map(&:to_i)
+
+prev_stdout = nil
+amplifiers = []
+NUM_AMPLIFIERS.times do |_i|
+  amplifiers << Computer.new(stdin: prev_stdout)
+  prev_stdout = amplifiers.last.stdout
+end
+amplifiers[0].stdin = amplifiers[-1].stdout
+
+highest = 0
+PHASE_RANGE.to_a.permutation do |phases|
+  (0...NUM_AMPLIFIERS).each do |i|
+    amplifiers[i].stdin << phases[i]
+  end
+  amplifiers[0].stdin << 0
+  threads = amplifiers.map do |amp|
+    Thread.new do
+      amp.execute(intcode)
+    end
+  end
+  threads.each { |t| t.join }
+  output = amplifiers.last.stdout.pop
+  highest = [highest, output].max
+end
+
+puts highest
