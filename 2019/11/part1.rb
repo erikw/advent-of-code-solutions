be95ce8308eff5b679335db2f0eeef581a91c990
Erik Westrup <erik.westrup@gmail.com>
2022-07-28 22:13:04 +0200

    Add 2019/11

---
+++ 2019/11/part1.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+
+require_relative '../09/computer'
+
+COLOR_BLACK = 0
+COLOR_WHITE = 1
+PANEL_BLACK = '.'
+PANEL_WHITE = '#'
+DIRECTION = { 0 => 1i, 1 => -1i }
+
+intcode = ARGF.readline.split(',').map(&:to_i)
+
+panels = Hash.new(COLOR_BLACK) # Coordinate => color
+dir = -1 # [-1, i, 1, -i] == [north, east, south, west]
+pos = 0 + 0i
+
+computer = Computer.new
+comp_thr = Thread.new { computer.execute(intcode) }
+
+while comp_thr.alive?
+  computer.stdin << panels[pos]
+  panels[pos] = computer.stdout.pop
+  dir *= DIRECTION[computer.stdout.pop]
+  pos += dir
+end
+
+puts panels.size
