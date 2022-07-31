f724da0b63343a00eb333dac656d684b409685a6
Erik Westrup <erik.westrup@gmail.com>
2022-07-31 14:24:57 +0200

    Add 2019/14

---
+++ 2019/14/part2.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib'
+
+ORES = 1_000_000_000_000
+
+inv_reactions = inv_reactions_from_input
+
+# The solution is simply not (ORES / fuel_cost(inv_reactions, 1).to_f).floor
+# because there might be reactions that pays off more in the long run when we need
+# more than 1 fuel.
+# Instead: binary search the solution!
+
+lower = (ORES / fuel_cost(inv_reactions, 1).to_f).floor.to_f
+upper = lower * 2 # Heuristic
+
+mid = nil
+cost = nil
+while lower <= upper
+  mid = ((upper - lower) / 2.0).floor + lower
+  cost = fuel_cost(inv_reactions, mid)
+  if cost < ORES
+    lower = mid + 1
+  elsif cost > ORES
+    upper = mid - 1
+  else
+    break
+  end
+end
+
+mid -= 1 if cost > ORES
+puts mid.to_i
