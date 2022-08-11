229349794e8c47bafca462f4b209f58b57c5551d
Erik Westrup <erik.westrup@gmail.com>
2022-08-11 21:13:41 +0200

    Add 2019/15

---
+++ 2019/15/part1.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# Algorithm
+# 1. Recursively discover the whole reachable map
+#   * Backtrack when recursion returns to step the robot back to the original position.
+# 2. Run Dijkstra on the map from origin to the found oxysys in previous system
+#   * No need for the intcomputer anymore in this step.
+
+require_relative 'lib'
+
+intcode = ARGF.readline.split(',').map(&:to_i)
+computer = Computer.new
+comp_thr = Thread.new { computer.execute(intcode) }
+
+map = Hash.new(SYM_UNKNOWN) # Complex coord => symbol
+map[POS_START] = SYM_OPEN
+
+explore(map, computer, POS_START)
+# print_map(map, POS_START)
+pos_oxysys = map.select { |_k, v| v == SYM_OXYSYS }.keys.first
+dist, _prev = dijkstra(map, POS_START, pos_oxysys)
+puts dist[pos_oxysys]
---
+++ 2019/15/part2.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# Algorithm:
+# * Find the map
+# * Flood oxygen with a modifed BFS that goes ring by ring to count the depth/level (= minutes).
+
+require_relative 'lib'
+
+intcode = ARGF.readline.split(',').map(&:to_i)
+computer = Computer.new
+comp_thr = Thread.new { computer.execute(intcode) }
+
+map = Hash.new(SYM_UNKNOWN) # Complex coord => symbol
+map[POS_START] = SYM_OPEN
+
+explore(map, computer, POS_START)
+pos_oxysys = map.select { |_k, v| v == SYM_OXYSYS }.keys.first
+num_open = map.count { |_k, v| v == SYM_OPEN }
+
+que = Thread::Queue.new
+que << pos_oxysys
+minutes = 0
+flooded = 0
+
+until flooded == num_open
+  layer_count = que.length
+  layer_count.times do
+    pos = que.pop
+    DIR2DELTA.values.each do |delta|
+      npos = pos + delta
+      next unless map.keys.include?(npos) && map[npos] == SYM_OPEN
+
+      map[npos] = SYM_OXYGEN
+      flooded += 1
+      que << npos
+    end
+  end
+  # print_map(map)
+  minutes += 1
+end
+
+puts minutes
