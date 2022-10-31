d83a7b543f6fb31c2986debf31791479ebf11254
Erik Westrup <erik.westrup@gmail.com>
2022-10-31 09:51:06 +0100

    Add js support for solve.sh

--- bin/solve_day.sh
+++ bin/solve_day.sh
@@ -1,34 +1,51 @@
 #!/usr/bin/env bash
-# Usage:
-# $ solve_day.sh
-# $ solve_day.sh yy/d
-# $ solve_day.sh yyyy/dd
-# TODO consider using https://github.com/GreenLightning/advent-of-code-downloader next year instead.
-#      Or even better https://pypi.org/project/advent-of-code-data/
 
-set -eu
+set -o errexit
+set -o nounset
+set -o pipefail
+[[ "${TRACE-0}" =~ ^1|true|yes$ ]] && set -o xtrace
 
+SCRIPT_NAME=${0##*/}
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-. $SCRIPT_DIR/lib.sh
-cd_git_root
+cd "$SCRIPT_DIR"
 
-FILES=(README.txt part1.rb input input1.0 output1.0 part2.rb)
+IFS= read -rd '' USAGE <<EOF || :
+Solve a puzzle day.
+Usage: $ ${SCRIPT_NAME} -h | [-l (rb|js)] [ datefmt ]
+
+Options:
+datefmt\t\t yy/d, yy/dd, yyyy/dd
+-l lang\t\t Language to use. Default: rb
+EOF
+
+declare -A HEADERS
+HEADERS[rb]=$(cat <<'HEADER'
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+HEADER
+)
+HEADERS[js]=$(cat <<'HEADER'
+#!/usr/bin/env node
+
+HEADER
+)
 
 enter_day() {
 	local year=$1
 	local day=$2
-	local files_arrayname=$3[@]
+	local file_ext=$3
+	local files_arrayname=$4[@]
 	local files=("${!files_arrayname}")
 
 	path="$year/$day"
 	mkdir -p $path
 	cd "$path"
 
-	touch ${files[@]}
-	chmod u+x *.rb
-	for r in *.rb; do
-		printf "#!/usr/bin/env ruby\n" >> $r
-		printf "# frozen_string_literal: true\n\n" >> $r
+	touch "${files[@]}"
+	chmod u+x *.${file_ext}
+	for f in *.${file_ext}; do
+		echo "${HEADERS[$file_ext]}" > $f
 	done
 }
 
@@ -52,6 +69,19 @@ fetch_input() {
 	curl --remote-name --remote-header-name --silent --fail --cookie "session=$AOC_SESSION" "$url"
 }
 
+
+
+arg_lang="rb"
+while getopts ":l:h?" opt; do
+	case "$opt" in
+		l) arg_lang="$OPTARG";;
+		:) echo "Option -$OPTARG requires an argument." >&2; exit 1;;
+		h|?|*) echo -e "$USAGE"; exit 0;;
+	esac
+done
+shift $(($OPTIND - 1))
+
+
 year=$(date +%Y)
 day=$(date +%d)
 if [ $# -eq 1 ]; then
@@ -63,10 +93,16 @@ if [ $# -eq 1 ]; then
 	test ${#day}  -eq 2 || day="0$day"
 fi
 
+files=(README.txt input input1.0 output1.0)
+files+=("part1.${arg_lang}")
+files+=("part2.${arg_lang}")
 
+
+. $SCRIPT_DIR/lib.sh
+cd_git_root
 load_dotenv
-enter_day $year $day FILES
-fetch_input $year $(echo $day | bc)
+enter_day $year $day $arg_lang files
+#fetch_input $year $(echo $day | bc)
 
 
 if [ -n "${TMUX+x}" ]; then
@@ -74,9 +110,8 @@ if [ -n "${TMUX+x}" ]; then
 	tmux last-pane
 fi
 
-#nvim -p ${FILES[@]}
 # vim alias not set to nvim, assume $EDITOR is a proper editor (=vi-like).
-$EDITOR -c "tabedit part1.rb | sp input | tabedit input1.0 | sp output1.0 | tabedit part2.rb | normal 2gt " README.txt
+$EDITOR -c "tabedit part1.${arg_lang} | sp input | tabedit input1.0 | sp output1.0 | tabedit part2.${arg_lang} | normal 2gt " README.txt
 
 
 cd_git_root
