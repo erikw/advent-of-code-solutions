ab11f734144565c194e842798b3a81a2b2f32b4b
Erik Westrup <erik.westrup@gmail.com>
2022-11-07 16:40:27 +0100

    Add 2021/02 js

---
+++ 2021/02/part2.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const fs = require('node:fs');
+
+var instructions = fs.readFileSync(process.argv[2]).toString().trim().split("\n")
+
+let pos = 0;
+let depth = 0;
+let aim = 0;
+
+instructions.forEach(instruction => {
+	let [cmd, val] = instruction.split(' ');
+	val = Number.parseInt(val, 10);
+
+	switch(cmd) {
+		case "forward":
+			pos += val;
+			depth += aim * val;
+		break;
+		case "down":
+			aim += val;
+		break;
+		case "up":
+			aim -= val;
+		break;
+	}
+});
+
+console.log(pos * depth);
