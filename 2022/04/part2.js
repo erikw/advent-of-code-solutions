17a1e90814d5a4528c53e4125fbdc17d2b94eb91
Erik Westrup <erik.westrup@gmail.com>
2022-12-04 17:39:11 +0100

    Add 2022/04 js

---
+++ 2022/04/part2.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+"use strict";
+
+import { readFileSync } from "node:fs";
+
+let pairRanges = readFileSync(process.argv[2])
+  .toString()
+  .trim()
+  .split("\n")
+  .map((line) => {
+    return Array.from(line.matchAll(/\d+/g), (d) => Number.parseInt(d, 10));
+  });
+
+let overlapping = pairRanges.filter(([start1, end1, start2, end2]) => {
+  return (
+    (start2 <= end1 && start2 >= start1) || (start1 <= end2 && start1 >= start2)
+  );
+}).length;
+
+console.log(overlapping);
