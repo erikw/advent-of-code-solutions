bd6f2e771ac4c8f9211a342ad5407844ba0b11c5
Erik Westrup <erik.westrup@gmail.com>
2022-12-06 08:24:17 +0100

    fd -0 ".*.js" | xargs -0 prettier -w

--- 2021/02/part2.js
+++ 2021/02/part2.js
@@ -1,28 +1,32 @@
 #!/usr/bin/env node
-const fs = require('node:fs');
+const fs = require("node:fs");
 
-var instructions = fs.readFileSync(process.argv[2]).toString().trim().split("\n")
+var instructions = fs
+  .readFileSync(process.argv[2])
+  .toString()
+  .trim()
+  .split("\n");
 
 let pos = 0;
 let depth = 0;
 let aim = 0;
 
-instructions.forEach(instruction => {
-	let [cmd, val] = instruction.split(' ');
-	val = Number.parseInt(val, 10);
+instructions.forEach((instruction) => {
+  let [cmd, val] = instruction.split(" ");
+  val = Number.parseInt(val, 10);
 
-	switch(cmd) {
-		case "forward":
-			pos += val;
-			depth += aim * val;
-		break;
-		case "down":
-			aim += val;
-		break;
-		case "up":
-			aim -= val;
-		break;
-	}
+  switch (cmd) {
+    case "forward":
+      pos += val;
+      depth += aim * val;
+      break;
+    case "down":
+      aim += val;
+      break;
+    case "up":
+      aim -= val;
+      break;
+  }
 });
 
 console.log(pos * depth);
