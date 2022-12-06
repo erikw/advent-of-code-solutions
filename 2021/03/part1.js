f521b05ab288748c48d351699cce541ead072500
Erik Westrup <erik.westrup@gmail.com>
2022-12-06 08:24:17 +0100

    fd -0 ".*.js" | xargs -0 prettier -w

--- 2021/03/part1.js
+++ 2021/03/part1.js
@@ -1,18 +1,23 @@
 #!/usr/bin/env node
 
-const fs = require('node:fs');
+const fs = require("node:fs");
 
-const transpose = matrix => matrix[0].map((x, i) => matrix.map(y => y[i]));
+const transpose = (matrix) => matrix[0].map((x, i) => matrix.map((y) => y[i]));
 
-var numbers = fs.readFileSync(process.argv[2]).toString().trim().split("\n").map(l => Array.from(l).map(v => Number.parseInt(v, 10)));
+var numbers = fs
+  .readFileSync(process.argv[2])
+  .toString()
+  .trim()
+  .split("\n")
+  .map((l) => Array.from(l).map((v) => Number.parseInt(v, 10)));
 numbers = transpose(numbers);
 
 let gamma_s = "";
 let epsilon_s = "";
-numbers.forEach(row => {
-	let sum = row.reduce((sum ,val) => sum + val, 0);
-	gamma_s += (sum >= row.length / 2) ? '1' : '0';
-    epsilon_s += gamma_s.at(-1) == '0' ? '1' : '0';
+numbers.forEach((row) => {
+  let sum = row.reduce((sum, val) => sum + val, 0);
+  gamma_s += sum >= row.length / 2 ? "1" : "0";
+  epsilon_s += gamma_s.at(-1) == "0" ? "1" : "0";
 });
 
 let gamma = Number.parseInt(gamma_s, 2);
--- 2022/01/part1.js
+++ 2022/01/part1.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
-const fs = require('node:fs');
+const fs = require("node:fs");
 
-let elf_calories = fs.readFileSync(process.argv[2]).toString().trim().split("\n\n").map(calories => {
-	return calories.split('\n').map(cal => Number.parseInt(cal, 10)).reduce((sum, cal) => sum + cal);
-});
+let elf_calories = fs
+  .readFileSync(process.argv[2])
+  .toString()
+  .trim()
+  .split("\n\n")
+  .map((calories) => {
+    return calories
+      .split("\n")
+      .map((cal) => Number.parseInt(cal, 10))
+      .reduce((sum, cal) => sum + cal);
+  });
 
 let max_cal = Math.max(...elf_calories);
 console.log(max_cal);
