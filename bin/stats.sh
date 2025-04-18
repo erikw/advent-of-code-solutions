7a9d6d406cc46053bc60a76161dab9d57e089f50
Erik Westrup <erik.westrup@icloud.com>
2025-04-18 06:57:19 +0000

    stats.sh now counts total stars

--- bin/stats.sh
+++ bin/stats.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Assumption: a README.txt means a solved puzzle.
+# Assumption: a part[1]2.* file means that part is considered solved.
 
 ESC_BOLD="\033[1m"
 ESC_REST="\033[0m"
@@ -9,19 +9,24 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd_git_root
 
 nbr_solved() {
-	local path="$1"
-	test -z ${path} && path=.
+	local path="./$1" # Unify so that path always starts with ./, so cut below works.
 
 	# Ref: https://stackoverflow.com/a/69326159/265508
-	find "$path" -name README.txt -exec printf %c {} + | wc -c | tr -d ' '
+	stars=$(find "$path" -regex '.*/part[12]\.\w+$' -print | cut -d. -f2 | sort -u | wc -l)
+
+	# Day 25 only as part1, part2 for free.
+	xmas_day_stars=$(find "$path" -regex '.*/25/part[12]\.\w+$' | cut -d. -f2 | sort -u | wc -l)
+	((stars+=$xmas_day_stars))
+
+	echo "$stars"
 }
 
-printf "Number of solved puzzle ${ESC_BOLD}days${ESC_REST}:\n"
+printf "Collected stars per year:\n"
 years=$(find . -maxdepth 1 -type d -regex "\./*[0-9]*" | grep -oE "[0-9]+" | sort)
 for year in $years; do
 	solved=$(nbr_solved $year)
-	test $solved = "25" && solved="25 ⭐️"
+	test $solved = "50" && solved="50 ⭐️"
 	printf "%s: %s\n" $year "$solved"
 done
 printf "===========\n"
-printf "Total: %d\n" $(nbr_solved)
+printf "Total: ${ESC_BOLD}%d${ESC_REST}\n" $(nbr_solved)
