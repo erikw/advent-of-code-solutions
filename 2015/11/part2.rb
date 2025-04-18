8e3cd8b0baf349f27c160b8d1c85aa30722c85db
Erik Westrup <erik.westrup@icloud.com>
2025-04-18 07:01:18 +0000

    Add lost 2015/11/part2.rb

---
+++ 2015/11/part2.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+def pw_valid(password)
+  triplet = password.chars.each_cons(3).map { |chrs| chrs.map(&:ord) }.any? do |a, b, c|
+    (a + 1) == b && b == (c - 1)
+  end
+  double = password.chars.each_with_index.each_cons(2).select do |a, b|
+             a[0] == b[0]
+           end.map { |a, b| [a[1], b[1]] }.reduce([]) do |acc, a|
+    if acc.empty? || acc.last.last != a[0]
+      acc << a
+    else
+      acc
+    end
+  end.length >= 2
+
+  triplet && double
+end
+
+def pw_next(password)
+  pos = password.length - 1
+  loop do
+    password[pos] = (((password[pos].ord + 1 - 'a'.ord) % ('z'.ord + 1 - 'a'.ord)) + 'a'.ord).chr
+    next if %w[i o l].include?(password[pos])
+    break if pos == 0 || password[pos] != 'a'
+
+    pos -= 1
+  end
+end
+
+password = ARGF.readline.chomp
+
+2.times do
+  loop do
+    pw_next(password)
+    break if pw_valid(password)
+  end
+end
+
+puts password
