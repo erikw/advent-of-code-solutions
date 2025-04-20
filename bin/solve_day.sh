10d434108d9e376274e92d70af00f51e05dcee3d
Erik Westrup <erik.westrup@icloud.com>
2025-04-20 06:04:08 +0000

    Spawn subshell in codespaces

--- bin/solve_day.sh
+++ bin/solve_day.sh
@@ -126,7 +126,9 @@ fetch_input $year $(echo $day | bc)
 
 
 if [ "$CODESPACES" = true ]; then
-	code --wait part1.${arg_lang} input input1.0 output1.0 part2.${arg_lang} README.txt
+	# code --wait part1.${arg_lang} input input1.0 output1.0 part2.${arg_lang} README.txt
+	code part1.${arg_lang} input input1.0 output1.0 part2.${arg_lang} README.txt
+	$SHELL # Spawn subshell in 20yy/mm
 else
 	if [ -n "${TMUX+x}" ]; then
 		tmux split-window -h
