f03780f251baea8023bcc1f68db356ee2d687bd9
Erik Westrup <erik.westrup@icloud.com>
2025-04-21 19:04:06 +0000

    Tried adding bin/ to PATH

--- bin/solve_day.sh
+++ bin/solve_day.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# TODO add bin/ to PATH in CODESPACES
-# 	TODO IF SO adding to PATH, prefix all scripts with aoc_
 # TODO NO command works with no arguments, should default to today's date. Hmm butit's not december. Maybe detect and exit then!
 
 set -o nounset
