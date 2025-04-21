be02d8b0df48dd776d0496f93a45c149485ab463
Erik Westrup <erik.westrup@icloud.com>
2025-04-21 12:21:49 +0000

    Prepare for README.md auto-creation

--- bin/empty_readmes.sh
+++ bin/empty_readmes.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 # Find READMEs that are empty, because sometimes I forget to paste in the instructions.
+# TODO archive this once solve_day.sh automatically creates the README...
+
+TARGET=README.md
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 . $SCRIPT_DIR/lib.sh
 cd_git_root
 
-git ls-files | grep README.txt | xargs -I {} sh -c 'wc -l {}' | grep "^\s*0\s" | awk '{print $2}'
+git ls-files | grep "$TARGET" | xargs -I {} sh -c 'wc -l {}' | grep "^\s*0\s" | awk '{print $2}'
