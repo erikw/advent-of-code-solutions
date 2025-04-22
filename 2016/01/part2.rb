af502e15f23e66895658aa71c8aa982abcaa6834
Erik Westrup <erik.westrup@icloud.com>
2025-04-22 09:40:35 +0000

    Remove incomplete solution.

--- 2016/01/part2.rb
+++ 2016/01/part2.rb
@@ -1,37 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'set'
+
 row = 0
 col = 0
 facing = 0
-lines = []
+visited = Set.new([row, col])
 ARGF.readline.split(', ').map {|p| [p[0], p[1..].to_i] }.each do |turn, dist|
   facing = (facing + (turn == 'R' ? 1 : -1)) % 4
-  rstart, cstart = row, col
+  row_inc, col_inc = 0, 0
   case facing
-  when 0 then row += dist.to_i  # N
-  when 1 then col += dist.to_i  # E
-  when 2 then row -= dist.to_i  # S
-  when 3 then col -= dist.to_i  # W
+  when 0 then row_inc = 1  # N
+  when 1 then col_inc = 1  # E
+  when 2 then row_inc = -1 # S
+  when 3 then col_inc = -1 # W
   end
-
-  line_new = [[rstart, cstart], [row, col]]
-  line_new_horiz = line_new[0][0] == line_new[1][0]
-  lines[0...-1].each do |line|
-    line_horiz = line[0][0] == line[1][0]
-    if line_new_horiz && line_horiz
-      ls = lines[0][1]
-      le = lines[1][1]
-      lns = lines_new[0][1]
-      lne = lines_new[1][1]
-      if ls.between?(lns, lne) || le.between?(lns, lne) || lns.between?(ls, le) || lne.between?(ls, le)
-        puts # TODO calc instersection point
-        return true
-      end
+  dist.times do
+    row += row_inc
+    col += col_inc
+    loc = [row, col]
+    if visited.include? loc
+      puts row.abs + col.abs
+      exit
     end
-
+    visited <<  loc
   end
-
-
-  lines << line_new
 end
