7de39ce1aae49e390c67493c9ce097eec7a009c6
Erik Westrup <erik.westrup@icloud.com>
2025-08-16 20:17:04 +0200

    converting to complex numbers

--- 2022/17/part1.py
+++ 2022/17/part1.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/]/env python3
 import fileinput
 import itertools
-from pprint import pprint
+from collections import defaultdict
 
 CHAMBER_WIDTH = 7
 TOTAL_ROCKS = 1
@@ -18,40 +18,43 @@ SYM_SPACE = "."
 SYM_ROCK_FALL = "@"
 SYM_ROCK_REST = "#"
 
+# ROCKS = [
+#     [[".", ".", "@", "@", "@", "@", "."]],
+#     [
+#         [".", ".", ".", "@", ".", ".", "."],
+#         [".", ".", "@", "@", "@", ".", "."],
+#         [".", ".", ".", "@", ".", ".", "."],
+#     ],
+#     [
+#         [".", ".", ".", ".", "@", ".", "."],
+#         [".", ".", ".", ".", "@", ".", "."],
+#         [".", ".", "@", "@", "@", ".", "."],
+#     ],
+#     [
+#         [".", ".", "@", ".", ".", ".", "."],
+#         [".", ".", "@", ".", ".", ".", "."],
+#         [".", ".", "@", ".", ".", ".", "."],
+#         [".", ".", "@", ".", ".", ".", "."],
+#     ],
+#     [
+#     [".", ".", "@", "@", ".", ".", "."],
+#     [".", ".", "@", "@", ".", ".", "."]],
+# ]
 ROCKS = [
-    [
-        ['.', '.', '@', '@', '@', '@', '.']
-    ],
-
-    [
-        ['.', '.', '@', '.', '.', '.', '.'],
-        ['.', '@', '@', '@', '.', '.', '.'],
-        ['.', '.', '@', '.', '.', '.', '.'],
-    ],
-    [
-        ['.', '.', '.', '.', '@', '.', '.'],
-        ['.', '.', '.', '.', '@', '.', '.'],
-        ['.', '.', '@', '@', '@', '.', '.'],
-    ],
-    [
-        ['.', '.', '@', '.', '.', '.', '.'],
-        ['.', '.', '@', '.', '.', '.', '.'],
-        ['.', '.', '@', '.', '.', '.', '.'],
-        ['.', '.', '@', '.', '.', '.', '.']
-    ],
-    [
-        ['.', '.', '@', '@', '.', '.', '.'],
-        ['.', '.', '@', '@', '.', '.', '.']
-    ],
+    set([2j, 3j, 4j, 5j]),
+    set([3j, 1 + 2j, 1 + 3j, 1 + 4j, 2 + 3j]),
+    set([2j, 3j, 4j, 1 + 4j, 2 + 4j]),
+    set([2j, 1 + 2j, 2 + 2j, 3 + 2j]),
+    set([2j, 3j, 1 + 2j, 1 + 3j]),
 ]
 
 
-PADDING = [
-    [SYM_SPACE] * CHAMBER_WIDTH
-] * ROCK_PAD_BOTTOM
+# PADDING = [[SYM_SPACE] * CHAMBER_WIDTH] * ROCK_PAD_BOTTOM
+
 
 def read_jet_pattern():
     return itertools.cycle(next(fileinput.input()).rstrip("\n"))
+
 
 def print_chamber(chamber):
     for row in reversed(chamber):
@@ -63,28 +66,25 @@ def print_chamber(chamber):
 def main():
     jet_pattern = read_jet_pattern()
     # chamber = [[SYM_SPACE] * CHAMBER_WIDTH]
-    chamber = []
+    chamber = defaultdict(lambda: SYM_SPACE)
     top = 0
 
     for rock_n in range(TOTAL_ROCKS):
         print(f"Rock number {rock_n:d} rock begins falling:")
 
-
-        chamber.extend(PADDING)
+        # chamber.extend(PADDING)
         # print_chamber(chamber)
 
-        rock = list(reversed(ROCKS[rock_n % len(ROCKS)]))
-        rock_height = len(rock)
-        chamber.extend(rock)
-        print_chamber(chamber)
+        rock = ROCKS[rock_n % len(ROCKS)]
+        # rock_height = len(rock)
+        # chamber.extend(rock)
+        # print_chamber(chamber)
+        rock_d = complex(ROCK_PAD_LEFT, top + ROCK_PAD_BOTTOM + 1)
+        rock = [c + rock_d for c in rock]  # TODO verify it works
 
-        while True:
-            jet = next(jet_pattern)
-            print(f"Jet of gas pushes rock {"left" if jet == JET_LEFT else "right"}:")
-
-
-
-
+        # while True:
+        #     jet = next(jet_pattern)
+        #     print(f"Jet of gas pushes rock {"left" if jet == JET_LEFT else "right"}:")
 
 
 if __name__ == "__main__":
