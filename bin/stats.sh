ea0a2373b079f7fa3fd87d6caf55e88c203d36ba
Erik Westrup <erik.westrup@icloud.com>
2025-10-29 21:19:09 +0100

    Prepare for 2025 nbr days change

--- bin/stats.sh
+++ bin/stats.sh
@@ -36,13 +36,15 @@ done
 shift $((OPTIND - 1))
 
 nbr_solved() {
-	local path="./${1:-}" # Unify so that path always starts with ./, so cut below works.
+	local year="${1:-}"
+	local path="./${year:-}" # Unify so that path always starts with ./, so cut below works.
 
 	# sort -u: account for the fact that a puzzle part can be solved in multiple languages.
 	# Regex written to work for both GNU/BSD find meaning e.g. `+` quantifier can't be used.
 	stars=$(find "$path" -iregex '.*/part[12]\.[a-z][a-z]*$' -print | cut -d. -f2 | sort -u | wc -l)
 
 	# Day 25 only as part1, part2 for free.
+	# TODO update this to check day12 instead if $year>=2025. Rename to stars_last_day
 	stars_d25=$(find "$path" -iregex '.*/25/part1\.[a-z][a-z]*$' | cut -d. -f2 | sort -u | wc -l)
 	((stars+=stars_d25))
 
